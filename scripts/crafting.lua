aa7d8a7986f4f44898069e817e7809a86ef14cd3
Kosay Jabre <kosay@thinkingbig.net>
2021-01-17 16:41:09 -0400

    Implemented craft_five_under_mouse

--- scripts/crafting.lua
+++ scripts/crafting.lua
@@ -5,4 +5,12 @@ local function craft_under_mouse(event)
     end
 end
 
+local function craft_five_under_mouse(event)
+    local player = game.players[event.player_index]
+    if player.selected then
+        player.begin_crafting{count=5, recipe=player.selected.name, silent=false}
+    end
+end
+
 script.on_event("craft-under-mouse", craft_under_mouse)
+script.on_event("craft-5-under-mouse", craft_under_mouse)
