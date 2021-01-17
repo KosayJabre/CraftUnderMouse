f69d9a75a3a8dda8c184dd159a53a8f8d3cd6250
Kosay Jabre <kosay@thinkingbig.net>
2021-01-17 16:14:40 -0400

    Added crafter prototype to data

---
+++ prototypes/crafter.lua
@@ -0,0 +1,12 @@
+data:extend {
+    {
+        type = 'custom-input',
+        name = 'craft-under-mouse',
+        key_sequence = 'ALT + Q'
+    },
+    {
+        type = 'custom-input',
+        name = 'craft-5-under-mouse',
+        key_sequence = 'ALT + SHIFT + Q'
+    }
+}
