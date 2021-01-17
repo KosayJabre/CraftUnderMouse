02ae6ab74b1de5c278bf996208d805657ec81991
Kosay Jabre <kosay@thinkingbig.net>
2021-01-17 16:14:25 -0400

    Added crafting.lua to controls

--- scripts/crafting.lua
+++ scripts/crafting.lua
@@ -1,11 +1,3 @@
-data:extend {
-    {
-        type = 'custom-input',
-        name = 'craft-under-mouse',
-        key_sequence = 'ALT + Q'
-    }
-}
-
 local function craft_under_mouse(event)
     local player = game.players[event.player_index]
     game.print("foo")
