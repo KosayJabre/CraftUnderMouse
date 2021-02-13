7d0d20f07401e45f679638eadd4fe5ec2bdea156
Kosay Jabre <kosay@thinkingbig.net>
2021-02-13 11:57:17 -0400

    Added setting for number of items to craft

---
+++ settings.lua
@@ -0,0 +1,9 @@
+data:extend({
+    {
+        type = "int-setting",
+        name = "multiple-craft-number",
+        setting_type = "runtime-per-user",
+        default_value = 5,
+        minimum_value = 2
+    }
+})
