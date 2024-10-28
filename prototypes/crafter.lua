0ccaf987aee61c612b5334e2ee956e2999ced1da
KosayJabre <Kosay.jabre@gmail.com>
2024-10-27 20:37:41 -0700

    Updated for Factorio 2.0

--- prototypes/crafter.lua
+++ prototypes/crafter.lua
@@ -3,10 +3,5 @@ data:extend {
         type = 'custom-input',
         name = 'craft-under-mouse',
         key_sequence = 'ALT + Q'
-    },
-    {
-        type = 'custom-input',
-        name = 'craft-multiple-under-mouse',
-        key_sequence = 'SHIFT + ALT + Q'
     }
 }
--- scripts/crafting.lua
+++ scripts/crafting.lua
@@ -12,21 +12,4 @@ local function craft_under_mouse(event)
     pcall(player.begin_crafting, {count=1, recipe=recipe_name})
 end
 
-
-
-local function craft_multiple_under_mouse(event)
-    local player = game.players[event.player_index]
-
-    if player.selected then 
-        if player.selected.name == "entity-ghost" then
-            recipe_name = player.selected.ghost_name
-        else
-            recipe_name = player.selected.name
-        end
-    end
-
-    pcall(player.begin_crafting, {count=settings.get_player_settings(event.player_index)["multiple-craft-number"].value, recipe=recipe_name})
-end
-
 script.on_event("craft-under-mouse", craft_under_mouse)
-script.on_event("craft-multiple-under-mouse", craft_multiple_under_mouse)
